b0e456a472c596195977812f5d083ba98972f346
Prasad-ganta <167187911+Prasad-ganta@users.noreply.github.com>
2026-05-24 17:11:29 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from api.routes import router
 
-import os
-
 app = FastAPI(title="Trip Optimizer API", version="1.0.0")
-
-# prevent runtime crashes if folders missing
-os.makedirs("data/raw", exist_ok=True)
-os.makedirs("data/matrices", exist_ok=True)
-os.makedirs("saved_models", exist_ok=True)
 
 app.add_middleware(
     CORSMiddleware,
